7815daf4cd7eef3d7e2800ea2a781456b0bd462a
Aycan iRiCAN <aycan@kudretofispc1.(none)>
2012-01-31 17:12:19 +0200

    Added setup script so that I can fast start developing on a new machine...

---
+++ setup-emacs.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+REPO=https://github.com/aycanirican/.emacs.d.git
+MONACO=http://jorrel.googlepages.com/Monaco_Linux.ttf
+FONTSDIR=~/.fonts
+
+installDeps () {
+    sudo apt-get install git-core ghc6 happy alex haskell-mode cabal-install
+}
+
+installFont () {
+    mkdir $FONTSDIR
+    wget $MONACO -O $FONTSDIR/Monaco.ttf
+    sudo fc-cache -f -v
+}
+
+installDotEmacs () {
+    cd ~/
+    git clone $REPO
+    echo "(load \"~/.emacs.d/bootstrap-init.el\")" >> ~/.emacs
+}
+
+installGhcMod () {
+    mkdir -p ~/elisp/ghcmod/
+    cabal install ghc-mod
+    cd ~/.cabal/share/ghc-mod-*
+    make
+    cp ghc* ~/elisp/ghcmod/
+}
+
+installDeps && installFont && installDotEmacs && installGhcMod
