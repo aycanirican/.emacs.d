169fb97acafb9d1984a9cdd40b0c66eee71e8fed
Aycan iRiCAN <iricanaycan@gmail.com>
2013-03-24 05:26:03 +0200

    Use the new features of haskell-mode

--- bootstrap-init.el
+++ bootstrap-init.el
@@ -1,4 +1,12 @@
 (setq emacs-start-time (current-time))
+(setq tramp-default-method "ssh"
+      tramp-chunksize 500)
+
+;; set all the different path implementations :(
+(let ((my-path (concat "/home/aycan/ghc762/bin:/home/aycan/.cabal/bin:" (getenv "PATH"))))
+    (setq eshell-path-env my-path)
+    (setq exec-path (append '("/home/aycan/ghc762/bin" "/home/aycan/.cabal/bin") exec-path))
+    (setenv "PATH" my-path))
 
 ;; start emacs server
 (server-start)
@@ -26,6 +34,7 @@
 (package-initialize)
 
 (when (not (string-equal "root" (getenv "USER")))
+  (load "~/.emacs.d/pivotaltracker-init")
   (load "~/.emacs.d/bindings-init")
   (load "~/.emacs.d/smex-init")
   (load "~/.emacs.d/w3m-init")
@@ -83,10 +92,6 @@
 
 ;; Eshell path fix. Toggle between eshell-currentwindow
 (require 'eshell)
-(defun eshell-mode-hook-func ()
-  (let ((my-path (concat "/home/aycan/ghc762/bin:/home/aycan/.cabal/bin/:" (getenv "PATH"))))
-    (setq eshell-path-env my-path)
-    (setenv "PATH" my-path)))
 
 (global-set-key 
  (kbd "C-M-e")
@@ -98,8 +103,6 @@
        (window-configuration-to-register :eshell-fullscreen)
        (eshell)
        (delete-other-windows)))))
-
-(add-hook 'eshell-mode-hook 'eshell-mode-hook-func)
 
 ;; Reset the UI to my norms
 (defun reset-ui (&optional frame)
@@ -119,3 +122,32 @@
   (set-window-buffer nil "*scratch*"))
 
 (reset-ui)
+
+
+;; flymake temporary file fix
+(defun flymake-create-temp-intemp (file-name prefix)
+  "Return file name in temporary directory for checking FILE-NAME.
+This is a replacement for `flymake-create-temp-inplace'. The
+difference is that it gives a file name in
+`temporary-file-directory' instead of the same directory as
+FILE-NAME.
+
+For the use of PREFIX see that function.
+
+Note that not making the temporary file in another directory
+\(like here) will not if the file you are checking depends on
+relative paths to other files \(for the type of checks flymake
+makes)."
+  (unless (stringp file-name)
+    (error "Invalid file-name"))
+  (or prefix
+      (setq prefix "flymake"))
+  (let* ((name (concat
+                (file-name-nondirectory
+                 (file-name-sans-extension file-name))
+                "_" prefix))
+         (ext  (concat "." (file-name-extension file-name)))
+         (temp-name (make-temp-file name nil ext))
+         )
+    (flymake-log 3 "create-temp-intemp: file=%s temp=%s" file-name temp-name)
+    temp-name))
--- haskell-init.el
+++ haskell-init.el
@@ -1,10 +1,8 @@
 ;;;; haskell-mode
-
 ;; Add .cabal to the PATH
 (setq exec-path (cons "~/.cabal/bin" exec-path))
 
 ;; maximum colors
-(global-font-lock-mode 1)
 (setq font-lock-maximum-decoration t)
 (setq haskell-font-lock-symbols t)
 
@@ -15,6 +13,33 @@
 
 ;; use spaces instead of tabs
 (setq-default indent-tabs-mode nil)
+
+;; haskell-interactive-mode
+(autoload 'notify "notify" "Notify TITLE, BODY." t)
+(autoload 'haskell-mode "haskell-mode" "haskell" t)
+(autoload 'literate-haskell-mode "haskell-mode" "literate haskell" t)
+
+(require 'inf-haskell)
+
+(custom-set-variables
+ '(haskell-process-type 'ghci)
+ '(haskell-notify-p t)
+ '(haskell-tags-on-save t)
+ '(haskell-stylish-on-save t)
+ '(haskell-ghci-program-args ("-read-dot-ghci")))
+
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'imenu-add-menubar-index)
+(add-hook 'haskell-mode-hook 'font-lock-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-decl-scan)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+(add-hook 'haskell-mode-hook 'haskell-hook)
+(add-hook 'haskell-cabal-mode-hook 'haskell-cabal-hook)
+
+(autoload 'switch-to-haskell "inf-haskell"
+ "Show the inferior-haskell buffer.  Start the process if needed." t nil)
+(autoload 'literate-haskell-mode "haskell-mode" "Major mode for editing literate Haskell scripts" t)
+
 (setq auto-mode-alist
       (append auto-mode-alist
               '(("\\.[hg]s$"  . haskell-mode)
@@ -22,28 +47,45 @@
                 ("\\.hsc$"     . haskell-mode)
 		("\\.chs$"    . haskell-mode)
                 ("\\.l[hg]s$" . literate-haskell-mode))))
-(autoload 'haskell-mode "haskell-mode" "Major mode for editing Haskell scripts" t)
-(autoload 'literate-haskell-mode "haskell-mode" "Major mode for editing literate Haskell scripts" t)
-
-(add-hook 'haskell-mode-hook 'turn-on-font-lock)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-decl-scan)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
-; (add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
-
-(require 'inf-haskell)
 
 ;(setq haskell-program-name "/usr/bin/ghci")
 ;(setq haskell-ghci-program-name "/usr/bin/ghci")
-(setq haskell-program-name "~/ghc762/bin/ghci")
-(setq haskell-ghci-program-name "~/ghc762/bin/ghci")
+;(setq haskell-program-name "~/ghc762/bin/ghci")
+;(setq haskell-ghci-program-name "~/ghc762/bin/ghci")
 
 (autoload 'run-ghci "haskell-ghci" "Go to the *ghci* buffer" t nil)
 (defalias 'run-haskell (quote switch-to-haskell))
 
-(autoload 'switch-to-haskell "inf-haskell"
-  "Show the inferior-haskell buffer.  Start the process if needed." t nil)
 
-(define-key haskell-mode-map (kbd "<f3>") (lambda()(interactive)(browse-url (format "http://holumbus.fh-wedel.de/hayoo/hayoo.html?query=%s&start" (current-word)))))
-(define-key haskell-mode-map (kbd "<f4>") (lambda()(interactive)(browse-url (format "http://www.haskell.org/hoogle/?hoogle=%s" (current-word)))))
+(defun haskell-hook ()
+  (define-key haskell-mode-map [?\C-c ?\C-z] 'haskell-interactive-switch)
+  (define-key haskell-mode-map (kbd "C-`") 'haskell-interactive-bring)
+  (define-key haskell-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+  (define-key haskell-mode-map (kbd "C-c c") 'haskell-process-cabal)
+  (define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
+  (define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
+  (define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)
+  (define-key haskell-mode-map [f8] 'haskell-navigate-imports)
 
+  ;; Jump to the definition of the current symbol.
+  (define-key haskell-mode-map (kbd "M-.") 'haskell-mode-tag-find)
+
+  ;; Indent the below lines on columns after the current column.
+  (define-key haskell-mode-map (kbd "C-<right>")
+    (lambda ()
+      (interactive)
+      (haskell-move-nested 1)))
+  ;; Same as above but backwards.
+  (define-key haskell-mode-map (kbd "C-<left>")
+    (lambda ()
+      (interactive)
+      (haskell-move-nested -1)))
+
+  (define-key haskell-mode-map (kbd "<f3>") (lambda()(interactive)(browse-url (format "http://holumbus.fh-wedel.de/hayoo/hayoo.html?query=%s&start" (current-word)))))
+  (define-key haskell-mode-map (kbd "<f4>") (lambda()(interactive)(browse-url (format "http://www.haskell.org/hoogle/?hoogle=%s" (current-word))))))
+
+(defun haskell-cabal-hook ()
+  (define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+  (define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal)
+  (define-key haskell-cabal-mode-map (kbd "C-`") 'haskell-interactive-bring)
+  (define-key haskell-cabal-mode-map [?\C-c ?\C-z] 'haskell-interactive-switch))
